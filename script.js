9bad1fc8e171ecd9637eb5156eb203ea2c78dd0a
Lucas Ferreira Lan <128648130+LucasFLan@users.noreply.github.com>
2024-11-18 11:49:47 -0300

    Atualização

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ const posts = [
       image: "/img/sorteio-diarias.jpg",
       content: "Recebi uma mensagem no WhatsApp dizendo que ganhei um sorteio. Eles pedem meus dados bancários para depósito. É seguro enviar?",
       answer: "danger",
-      feedback: "Atente-se, alta chance ser ser golpe, verifique se você realmente participou de algum sorteio ultimamente, nunca passe seus dados pessoais para desconhecidos."
+      feedback: "Atente-se, alta chance ser ser golpe, verifique se você realmente participou de algum sorteio ultimamente, NUNCA passe seus dados pessoais para desconhecidos."
     },
     {
       image: "/img/email.png",
       content: "Recebi um email suspeito, dizendo que alguém tentou acessar minha conta e pedindo para redefinir minha senha pelo link. Porém, percebo que o endereço de email não é correspondente ao oficial, além disso não tenho conta nesse site, portanto excluo e ignoro o email, SEM CLICAR no link",
       answer: "safe",
-      feedback: "Correto! Muita atenção a esse tipo de email com links, nunca clique diretamente no link, além disso, verifique o endereço de email do remetente para comprovar que é oficial"
+      feedback: "Muita atenção a esse tipo de email com links, NUNCA clique diretamente no link, além disso, verifique o endereço de email do remetente para comprovar que é oficial"
     },
     {
       image: "/img/holi.jpg",
@@ -24,16 +24,16 @@ const posts = [
       feedback: "Cuidado! Ao expor esse tipo de informação sensível, como a sua localização, você fica vulnerável a ataques e crimes"
     },
     {
-      image: "/img/crianca.png",
-      content: "Postar fotos de crianças nas redes sociais",
+      image: "/img/criança_jpg.webp",
+      content: "Postar fotos de crianças nas redes sociais. Expondo a criança para hackers e pedófilos, colocando sua segurança em risco.",
       answer: "danger",
-      feedback: "Atenção! Ao compartilhar fotos de crianças nas redes sociais, você torna a imagem dessa criança vulnerável a crimes, então cuidado ao expor!"
+      feedback: "Atenção! Ao compartilhar fotos de crianças nas redes sociais, você torna a imagem dessa criança vulnerável a crimes, além disso a imagem da criança pode ser facilmente utilizada para fins de exploração comercial ou criação de perfis falsos. então cuidado ao expor!"
     },
     {
       image: "/img/selfie.png",
-      content: "Antes de postar uma foto, verifico se não houve nenhum vazamento de dados pessoais, como vazamento do rg, cpf, endereço, entre outros dados sensíveis.",
+      content: "Antes de postar uma foto, VERIFICO se não houve nenhum vazamento de dados pessoais, como vazamento do rg, cpf, endereço, entre outros dados sensíveis.",
       answer: "safe",
-      feedback: "Correto! Antes de fazer qualquer postagem, verifique se não houve nenhum vazamento que possa lhe comprometer"
+      feedback: "Antes de fazer qualquer postagem, verifique se não houve nenhum vazamento que possa lhe comprometer"
     },
     {
       image: "/img/golpe.jpeg",
@@ -43,9 +43,21 @@ const posts = [
     },
     {
       image: "/img/crianca_escola.png",
-      content: "Postar uma foto do meu filho na escola, com o uniforme visível",
+      content: "Postar uma foto do meu filho na escola, com o UNIFORME VISÍVEL",
       answer: "danger",
       feedback: "ATENÇÃO! Ao compartilhar uma foto do seu filho com o uniforme da escola, pode expor a localização e rotina da criança, colocando-a em risco"
+    },
+    {
+      image: "/img/spam.jpeg",
+      content: "Você recebe a seguinte mensagem: Seu celular está vulnerável! Clique aqui para instalar a atualização de segurança urgente. Você NÃO clica pois identifica uma possível suspeita:",
+      answer: "safe",
+      feedback: "Cuidado! Este tipo de anúncio pode redirecionar a vítima para o download de arquivos NÃO SEGUROS disfarçado de atualização, permitindo que hackers tenham acesso aos dados da vítima"
+    },
+    {
+      image: "/img/spam-2.png",
+      content: "Você recebe um email enviado por uma casa de aposta DIRECIONANDO para cadastrar e depositar dinheiro na platarforma e em seguida começar a jogar",
+      answer: "danger",
+      feedback: "ATENÇÃO! Cuidado ao receber esses supostos link's pois podem direcionar para uma página falsa que simula um site, onde os usuários inserem seus dados bancários, como senhas, números de conta e dados do cartão de crédito."
     },
   ];
 
@@ -77,7 +89,7 @@ const posts = [
     feedbackElement.style.display = "block";
     document.getElementById("score").innerText = `Pontuação: ${score}`;
     currentPost++;
-    setTimeout(loadPost, 3300);
+    setTimeout(loadPost, 6000);
   }
 
   function endGame() {
